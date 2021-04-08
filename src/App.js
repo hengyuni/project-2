55803b412fb5d8ea5566f6303666288c30b676d3
hengyuni <hengyuni@gmail.com>
2021-04-08 00:12:50 -0400

    TYPES TYPES TYPES

--- src/App.js
+++ src/App.js
@@ -44,3 +44,7 @@ export default function App () {
     </Switch>
   );
 }
+
+// add nav bar
+// add footer
+// add search engine?
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -12,11 +12,18 @@ const Pokemon = (props) => {
   // console.log(mockData)
 
   const [pokemonData, setPokemonData] = useState(mockData)
+  const [deez, setDeez] = useState([])
 
   const getData = async () => {
+    let temp = null;
     try {
       const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
       const data = await res.json();
+      if(data.types.length === 1) {
+        setDeez(<p>Type: {data.types[0].type.name.toUpperCase()}</p>);
+      } else {
+        setDeez(<p>Types: {data.types[0].type.name.toUpperCase()}/{data.types[1].type.name.toUpperCase()}</p>);
+      }
       setPokemonData(data)
     } catch (err) {
       console.log(err)
@@ -35,7 +42,9 @@ const Pokemon = (props) => {
     )
   }
 
-  // const typeTwo = () => {
+
+
+  // let getDeez = () => {
   //   if(pokemonData.types[1] !== "undefined") {
   //     return <p>{pokemonData.type[1].type.name}</p>
   //   }
@@ -44,19 +53,22 @@ const Pokemon = (props) => {
   // let secondType = pokemonData.type[1].type.name;
   // let setSecondType = secondType !== "undefined" ? secondType : null;
 
+  console.log(pokemonData.types);
   return (
     <div className="App">
       <h1>{pokemonId}</h1>
       <img src={pokemonData.sprites.front_default} alt="pokemon front" width="150px"/>
        {/* <Sprite pokemon={pokemonId} /> */}
       {/* <h1>{pokemonId}</h1> */}
-      {pokemonData.types.map((type, i) => {
+      {/* {pokemonData.types.map((type, i) => {
+        // figure out to show types on one line!!!
         return (
           <p className={type.type.name} key={i}>
             Type: {type.type.name}
           </p>
         )
-      })}
+      })} */}
+      {deez}
       <p>Height: {pokemonData.height}</p>
       <p>Weight: {pokemonData.weight}</p>
     </div>
